2906399418a50f8204600e78018c6670adf34069
Abdillah7997 <ariabdillah009@gmail.com>
2020-11-15 08:15:45 +0700

    adminfinder

--- adminfinder.py
+++ adminfinder.py
@@ -1,38 +1,40 @@
-#!usr/bin/env python
-#coding: UTF-*
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
 from urllib2 import Request, urlopen, URLError, HTTPError
 
 def Cetak(a):
-    i = 0
-    while i<=a:
-        print " ",
-        i+=1
+	i = 0
+	while i<=a:
+		print " ",
+		i+=1
 
-def Creator():
-    Cetak(9); print "++++++++++++++++++++++++++++++"
-    Cetak(9); print "+     Tools Admin Finder     +"
-    Cetak(9); print "+  Tanggal Dibuat 14/11/2020 +"
-    Cetak(9); print "+     Creator : Abdillah     +"
-    Cetak(9); print "++++++++++++++++++++++++++++++"
-    Creator()
-    print "\n\n"
-def Admin():
-    find = open("kumpulanlink.txt","r"):
-        link = raw_input("Masukan URL Target (Contoh : www.contohnya.com \t: "):
-    print "\n\nBerhasil nih\t: \n"
-    while True:
-        sub_link = find.readline()
-        if not sub_link:
-            break
-        request_link = "http://"+link+"/"+sub_link
-        request = Request(request_link)
-        try:
-            response = urlopen(request)
-        except HTTPError as gagal:
-            continue
-        except URLError as gagal:
-            continue
-        else:
-            print "LINK YANG ADMIN =>>",request_link
-Admin()
+
+def CariAdmin():
+	teks = open("kumpulanlink.txt","r");
+	link = raw_input("Masukan URL Target (CONTOH : www.contoh.com)\t: ")
+	print "\n\nLink ==>\n"
+	while True:
+		sub = teks.readline()
+		if not sub:
+			break
+		req_link = "http://"+link+"/"+sub
+		req = Request(req_link)
+		try:
+			response = urlopen(req)
+		except HTTPError as e:
+			continue
+		except URLError as e:
+			continue
+		else:
+			print "LINK YANG BISA DIAKSES ",req_link
+
+def DiCetak():
+	Cetak(9); print "+++++++++++++++++++++++++++++++++++++"
+	Cetak(9); print "+      Recoded By Abdillah7997      +"
+	Cetak(9); print "+         From Zero To Hero         +"
+	Cetak(9); print "+         SEKIREI EXPLOITER         +"
+	Cetak(9); print "+++++++++++++++++++++++++++++++++++++"
+
+DiCetak()
+CariAdmin()
